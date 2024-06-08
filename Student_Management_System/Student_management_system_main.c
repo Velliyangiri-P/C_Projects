6a95aff91bdd6b7051f237d7935f19651c5f1a85
Velliyangiri <velliyangiri.mailbox@gmail.com>
2024-06-08 18:03:13 +0530

    Added code for Console-based Menu

--- Student_Management_System/Student_management_system_main.c
+++ Student_Management_System/Student_management_system_main.c
@@ -1,7 +1,32 @@
 #include "stdio.h"
+#include "stdint.h"
+#include "stdlib.h"
+
+void PrintMenu(void);
 
 int main (void)
 {
-    printf("Helllo !world\r\n");
+    //clears the screen
+    system("cls");
+
+    PrintMenu();
+
+    printf("Enter a number of option\r\n");
+    uint32_t Option=0;
+    scanf("%d", &Option);
+
+    printf("Helllo !world %d\r\n",Option);
     return 0;
+}
+
+void PrintMenu(void)
+{
+    printf("Select a option from below:\r\n" 
+            "1. Add a student\r\n"  
+            "2. Delete a student\r\n"
+            "3. Search for a student\r\n"
+            "4. Display all student\r\n"
+            "5. Save all student data\r\n"
+            "6. Load student data from a file\r\n"
+            );
 }
